6321ffd36e339a8681441ec888567d3aba62e5d4
AjinkyaKalaskar <ajinkya.kalaskar@ibm.com>
2022-04-14 14:34:59 +0530

    SettingUp Package and DI structure

---
+++ app/src/main/java/com/ajinkya/weather_forecast/WeatherApplication.kt
@@ -0,0 +1,7 @@
+package com.ajinkya.weather_forecast
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WeatherApplication : Application()
---
+++ app/src/main/java/com/ajinkya/weather_forecast/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.ajinkya.weather_forecast.util
+
+object Constants {
+    const val api_key = "d2b24fc28d097f45ff52eca08e168a35"
+}
